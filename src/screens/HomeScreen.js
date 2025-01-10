fa2fb7d294ac35ecf8945d838372aab251bf8434
petr-prikryl <petrik.prikryl@gmail.com>
2025-01-10 18:02:46 +0100

    skoro prod

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import { getPlaces } from '../utils/storage'; // Funkce pro načítání míst
+
+const defaultIcon = new Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 
 const HomeScreen = () => {
     const [places, setPlaces] = useState([]);
@@ -16,6 +28,33 @@ const HomeScreen = () => {
     return (
         <div>
             <h1>Seznam míst</h1>
+
+            {/* Zobrazení mapy s markery všech míst */}
+            <MapContainer
+                center={[50, 14]} // Střed mapy - můžete upravit podle potřeby
+                zoom={6}
+                style={{ height: '400px', width: '100%', marginBottom: '20px' }}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {places.map((place) => (
+                    <Marker
+                        key={place.id}
+                        position={[place.location.lat, place.location.lng]}
+                        icon={defaultIcon}
+                    >
+                        <Popup>
+                            <strong>{place.name}</strong>
+                            <br />
+                            <Link to={`/place/${place.id}`}>Zobrazit detail</Link>
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+
+            {/* Seznam míst */}
             <ul>
                 {places.map((place) => (
                     <li key={place.id}>
@@ -23,6 +62,7 @@ const HomeScreen = () => {
                     </li>
                 ))}
             </ul>
+
             <Link to="/add">Přidat nové místo</Link>
         </div>
     );
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ const API_KEY = '821e84eb0383552dbdde1dcb8677fd02';
 // Získá předpověď počasí pro zadané souřadnice
 export async function getWeather(lat, lng) {
     const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&lang=cz&appid=${API_KEY}`
     );
     const data = await response.json();
     return data;
